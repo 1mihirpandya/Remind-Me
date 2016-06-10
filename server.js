7f3f15354eec5139656df0055c2d9ed09f419b8c
Ruchi Pandya <Ruchi@Ruchis-Air.arc.nasa.gov>
2016-06-10 12:52:42 -0700

    mark 5

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', function (req, res) {
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
+    console.log('works');
     if (req.query['hub.verify_token'] === 'ultramagnus_prime') {
         console.log('works');
         res.send(req.query['hub.challenge']);
