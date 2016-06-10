d5c5cc20f1574e04ff19d92fcab10dd31c4b8f71
Ruchi Pandya <Ruchi@Ruchis-Air.arc.nasa.gov>
2016-06-10 13:17:57 -0700

    response test 2

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
+        if (event.message && (event.message.text === 'hello') {
+            sendMessage(event.sender.id, {text: "To add something to you list of reminders, begin your message with the word 'add'. To remove something from your list of reminders, begin your message with the word 'remove'."});
+        }
         if (event.message && event.message.text) {
             sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         }
