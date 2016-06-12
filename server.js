3485992dec99b71aedd2c141a9245ce76a83b2a8
Ruchi Pandya <Ruchi@Ruchis-Air.attlocal.net>
2016-06-12 13:02:37 -0700

    summary additions

--- server.js
+++ server.js
@@ -44,6 +44,16 @@ function convert_to_list()
 
 
 
+function convert_to_summary(val)
+{
+    listed = "";
+    for (i = 0; i < item_descriptions[val].length; i++)
+    {
+        listed = listed + "-" + item_descriptions[val][i] + "\n"; 
+    }
+    return listed;
+}
+
 
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
@@ -79,6 +89,7 @@ app.post('/webhook', function (req, res) {
                 if (index < items.length) 
                 {
                     items.splice(index, 1);
+                    item_descriptions.splice(index, 1);
                     sendMessage(event.sender.id, {text: "Item removed!"});
                 }
                 else
@@ -89,6 +100,12 @@ app.post('/webhook', function (req, res) {
             else if (((event.message.text).trim()).trim() === "list")
             {
                 to_do_list = convert_to_list();
+                sendMessage(event.sender.id, {text: to_do_list + ""});
+            }
+            else if (((event.message.text).trim()).substr(0,8) === "describe")
+            {
+                index = parseInt(((event.message.text).split(" "))[1]) - 1;
+                to_do_list = convert_to_summary(index);
                 sendMessage(event.sender.id, {text: to_do_list + ""});
             }
         }
