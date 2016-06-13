9db07b76c7ea6087908b0102f898f4c8f5677158
Ruchi Pandya <Ruchi@Ruchis-Air.attlocal.net>
2016-06-12 22:56:17 -0700

    summary additions

--- server.js
+++ server.js
@@ -78,12 +78,12 @@ app.post('/webhook', function (req, res) {
             else if (((event.message.text).trim()).substr(0,12) === "instructions")
                 {
                     sendMessage(event.sender.id, {text: "here already!"});
-                    sendMessage(event.sender.id, {text: "To add an item to your to-do list, simply type 'add' before the item's title. \
-                    To remove an item from your to-do list, simply type 'remove' followed by the item's number on the list. \
-                    You can see the entire list and their corresponding numbers by typing 'list'. \
-                    To add a description for an item, type 'add summary for' followed by the item number and a colon. \
-                    To find out what the description for an item is, type 'describe' followed by the item number. \
-                    To clear the entire list, type 'clear'."});
+                    sendMessage(event.sender.id, {text: "To add an item to your to-do list, simply type 'add' before the item's title. " +
+                    "To remove an item from your to-do list, simply type 'remove' followed by the item's number on the list. " +
+                    "You can see the entire list and their corresponding numbers by typing 'list'. " +
+                    "To add a description for an item, type 'add summary for' followed by the item number and a colon. " +
+                    "To find out what the description for an item is, type 'describe' followed by the item number. " +
+                    "To clear the entire list, type 'clear'."});
                 }
             else if ((event.message.text).substr(0,3) === "add")
             {
