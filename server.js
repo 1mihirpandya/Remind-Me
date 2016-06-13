4a864d8410b2919813542cc2dab9e17f99a385c3
Ruchi Pandya <Ruchi@Ruchis-Air.attlocal.net>
2016-06-12 17:44:14 -0700

    summary additions

--- server.js
+++ server.js
@@ -22,12 +22,91 @@ app.get('/webhook', function (req, res) {
 });
 
 
+var items = [];
+var item_descriptions = [];
+
+
+function convert_to_list()
+{
+    listed = "";
+    if (items.length == 0)
+        {
+            return "Empty list!"
+        }
+    for (i = 0; i < items.length; i++)
+    {
+        item_number = i + 1;
+        listed = listed + String(item_number) + "." + items[i] + "\n"; 
+    }
+    return listed;
+}
+
+
+
+function convert_to_summary(val)
+{
+    listed = "";
+    for (i = 0; i < item_descriptions[val].length; i++)
+    {
+        listed = listed + "-" + item_descriptions[val][i] + "\n"; 
+    }
+    return listed;
+}
+
+
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            
+            
+            
+            
+            
+            if ((event.message.text).substr(0,15) === "add summary for")
+            {
+                index = parseInt(((event.message.text).substr(((event.message.text).indexOf("for") + 3),((event.message.text).indexOf(":"))).trim())) - 1;
+                item_descriptions[index].push(((event.message.text).substr((event.message.text).indexOf(":") + 1)).trim());
+                sendMessage(event.sender.id, {text: "Summary added!"});
+            }
+            
+            
+            
+            
+            
+            
+            else if ((event.message.text).substr(0,3) === "add")
+            {
+                items.push((event.message.text).substr(3));
+                item_descriptions.push([]);
+                sendMessage(event.sender.id, {text: "Item added!"});
+            }
+            else if (((event.message.text).trim()).substr(0,6) === "remove")
+            {
+                index = parseInt(((event.message.text).split(" "))[1]) - 1;
+                if (index < items.length) 
+                {
+                    items.splice(index, 1);
+                    item_descriptions.splice(index, 1);
+                    sendMessage(event.sender.id, {text: "Item removed!"});
+                }
+                else
+                {
+                    sendMessage(event.sender.id, {text: "Sorry! That item doesn't exist."});
+                }
+            }
+            else if (((event.message.text).trim()).trim() === "list")
+            {
+                to_do_list = convert_to_list();
+                sendMessage(event.sender.id, {text: to_do_list + ""});
+            }
+            else if (((event.message.text).trim()).substr(0,8) === "describe")
+            {
+                index = parseInt(((event.message.text).split(" "))[1]) - 1;
+                to_do_list = convert_to_summary(index);
+                sendMessage(event.sender.id, {text: to_do_list + ""});
+            }
         }
     }
     res.sendStatus(200);
